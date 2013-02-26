3a65a2615560afd584e686297dcb2506cb49264f
Stanley Gu <stanleygu@gmail.com>
2013-02-26 00:58:41 +0000

    removed blank entry from components.json

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
     http = require('http'),
     path = require('path');
 var app = express();
-app.configure(function () {
+app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -21,12 +21,12 @@ app.configure(function () {
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
-app.configure('development', function () {
+app.configure('development', function() {
     app.use(express.errorHandler());
 });
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/sbml2matlab', sbml2matlab.translate);
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
