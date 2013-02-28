bce0a558160354ec41e5fa4cf7120d4798840752
Stanley Gu <stanleygu@gmail.com>
2013-02-28 09:42:03 +0000

    Added pretty printing of html from jade

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.configure(function() {
 });
 app.configure('development', function() {
     app.use(express.errorHandler());
+	app.locals.pretty = true;
 });
 app.get('/', routes.index);
 app.get('/users', user.list);
