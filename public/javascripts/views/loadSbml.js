788c2bf14ad0bd9a782aac075fa528cd8db95cdc
Stanley Gu <stanleygu@gmail.com>
2013-03-04 13:38:37 -0800

    added ace editor is working

--- public/javascripts/views/loadSbml.js
+++ public/javascripts/views/loadSbml.js
@@ -1,11 +1,20 @@
-/*global define*/
-define(['jquery', 'underscore', 'backbone', 'ace/ace'], function($, _, Backbone, ace) {
-	'use strict';
+/*global define ace*/
+define([
+	'jquery',
+	'underscore',
+	'backbone'
+],
+	function($, _, Backbone) {
+		'use strict';
 
-	var LoadSbmlView = Backbone.View.extend({
-		initialize: function () {
-			ace.edit(this.el);
-		}
+		var LoadSbmlView = Backbone.View.extend({
+			height: 800,
+			width: 400,
+			initialize: function () {
+				$(this.el).height(this.height);
+				$(this.el).width(this.width);
+				ace.edit(this.el);
+			}
+		});
+		return LoadSbmlView;
 	});
-	return LoadSbmlView;
-});
