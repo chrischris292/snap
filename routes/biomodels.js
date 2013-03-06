574f6ea2c49d98ff25d2a3ad1f8663de88c60190
Stanley Gu <stanleygu@gmail.com>
2013-03-06 10:42:53 -0800

    using express req.query

--- routes/biomodels.js
+++ routes/biomodels.js
@@ -3,8 +3,9 @@ exports.getModel = function (req, res) {
 	// Dependencies
 	var biomodels = require('biomodels').BioModelsWSClient;
 	// Get SBML From ID
-	console.log(biomodels.getModelSBMLById(req.body.id, function (err, results) {
+	console.log('requested ID: ' + req.query.id)
+	console.log(biomodels.getModelSBMLById(req.query.id, function (err, results) {
 		console.log(err + results);
+		res.send(err + results);
 	}));
-	res.send('done!');
 };
