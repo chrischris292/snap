0ac075a60d0a843d5f0ef3e6f85c5a0f738f2cd6
Stanley Gu <stanleygu@gmail.com>
2013-03-13 13:49:42 -0700

    moved y_axis above chart

--- public/javascripts/views/chart.js
+++ public/javascripts/views/chart.js
@@ -9,13 +9,12 @@ define([
 	'use strict';
 	var ChartView = Backbone.View.extend({
 		initialize: function () {
-			this.chartElement = document.createElement('div');
-			this.chartElement.setAttribute('id', 'chart_container');
-			$(this.el).append(this.chartElement);
 			this.yElement = document.createElement('div');
 			this.yElement.setAttribute('id', 'y_axis');
 			$(this.el).append(this.yElement);
-
+			this.chartElement = document.createElement('div');
+			this.chartElement.setAttribute('id', 'chart_container');
+			$(this.el).append(this.chartElement);
 			this.graph = new Rickshaw.Graph({
 				element: this.chartElement,
 				series: [{
@@ -26,10 +25,12 @@ define([
 						{ x: 2, y: 38 },
 						{ x: 3, y: 30 },
 						{ x: 4, y: 32 } ]
-				}]
-			});
+				}],
+				width: 100,
+				height: 100
+		});
 
-			//$('#y_axis').css('position', 'absolute').css('top', 0).css('bottom', 0).css('width', '40px');
+			//$('#y_axis').css('position', 'relative').css('top', 0).css('bottom', 0).css('width', '40px');
 			//$(this.el).css('position', 'relative')
 			//this.x_axis = new Rickshaw.Graph.Axis.Time({ graph: this.graph });
 			this.y_axis = new Rickshaw.Graph.Axis.Y({
