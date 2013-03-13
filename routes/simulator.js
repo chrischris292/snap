6bd620716ef644bcd06aeadac4e0561eccc15ac9
Stanley Gu <stanleygu@gmail.com>
2013-03-13 00:54:14 -0700

    Correctly calling libsbmlsim

--- routes/simulator.js
+++ routes/simulator.js
@@ -3,15 +3,44 @@ exports.libsbmlsim = function (req, res) {
 	'use strict';
 
 	var sim = require('libsbmlsimjs').libsbmlsim,
-		fs = require('fs');
+		fs = require('fs'),
+		exec = require('child_process').exec,
+		csv = require('ya-csv'),
+		simData;
 
 	fs.writeFile('tmp/temp.sbml', req.body.sbml, function (err) {
 		if (err) {
 			console.log(err);
 		} else {
 			console.log('SBML file saved in temporary location!');
+			//var result = sim.simulateSBMLFromFile('tmp/temp.sbml', 100, 0.1, 10, 0, 1, 0);
+			//sim.print_result(result);
+			//sim.print_result(result);
+			//var command = 'cd tmp && simulateSBML -t 100 -s 100 -m 1 temp.sbml',
+			var command = 'simulateSBML -t 100 -s 100 -m 1 temp.sbml',
+				options = {
+					cwd: '/home/stanleyg/snap/tmp'
+				};
+			exec(command, options, function (error, stdout, stderr) {
+				if (error) {
+					console.log('Error in executing child process: ' + error);
+				} else if (stderr) {
+					console.log('Error in simulation: ' + stderr);
+				} else {
+					console.log('Simulation Successful: ' + stdout);
+					var data = [],
+						reader;
+					console.log('Reading simulated data');
+					reader = csv.createCsvFileReader('tmp/out.csv').on('data', function (row) {
+						data.push(row);
+					}).on('end', function () {
+						console.log(data);
+						res.send(data);
+					});
+				}
+			});
+		//	fs.unlink('tmp/temp.sbml'); // delete temporary file
 
-			fs.unlink('tmp/temp.sbml'); // delete temporary file
 		}
 	});
 
