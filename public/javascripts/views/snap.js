86bb621b9f1e54a7c3488976603990ab1754fb2f
Stanley Gu <stanleygu@gmail.com>
2013-03-19 03:57:55 +0000

    removed old code and reorganized section

--- public/javascripts/views/snap.js
+++ public/javascripts/views/snap.js
@@ -60,16 +60,10 @@ define([
 			//this.biomodels.fetch();
 
 			// Simulation
+			this.$elSim = this.$el.children.find('div#sim');
+
+			// Chart
 			this.$elChart = this.$el.children().find('div#chart');
-			this.series = [{
-				color: 'steelblue',
-				data: [
-					{ x: 0, y: 40 },
-					{ x: 1, y: 49 },
-					{ x: 2, y: 38 },
-					{ x: 3, y: 30 },
-					{ x: 4, y: 32 } ]
-			}];
 			this.render();
 		},
 		events: {
