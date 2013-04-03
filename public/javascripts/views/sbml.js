5bf603c785f6c4f78b9e0aae4f98d942f438a061
Stanley Gu <stanleygu@gmail.com>
2013-04-03 15:41:37 +0000

    Use a template to show tmp layout picture

--- public/javascripts/views/sbml.js
+++ public/javascripts/views/sbml.js
@@ -2,7 +2,7 @@
 define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 	'use strict';
 	var SbmlView = Backbone.View.extend({
-		template: _.template('<button class="btn" id="<%= modelId %>"><%= modelId %></button>'),
+		template: _.template('<img src="<%= url %>" />'),
 		render: function() {
 			var that = this;
 			var sbml = this.model.get('sbml')
@@ -15,6 +15,9 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 				url: 'graphfab',
 				success: function (data, textStatus, jqXHR) {
 					console.log('Called GraphFab on Model');
+					that.$el.html(that.template({
+						url: '/show'
+					}))
 				}
 			});
 		}
