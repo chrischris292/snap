ca11cc4fab2160d6c4a30e7b2931adf8fbe517a7
Stanley Gu <stanleygu@gmail.com>
2013-04-03 16:14:13 +0000

    skeleton file for force layout view

---
+++ public/javascripts/views/force.js
@@ -0,0 +1,20 @@
+/*global define*/
+define([
+	'jquery',
+	'underscore',
+	'backbone'
+], function ($, _, Backbone) {
+	'use strict';
+
+	var ForceView = Backbone.View.extend({
+
+		initialize: function () {
+		},
+
+		render: function () {
+
+		}
+	});
+
+	return ForceView;
+});
