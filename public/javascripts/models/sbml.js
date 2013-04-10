013c8852694af20f1e4fd9836e86c55815b3c108
Stanley Gu <stanleygu@gmail.com>
2013-04-10 18:12:48 +0000

    Don't think these defaults are neccessary, but added validate

--- public/javascripts/models/sbml.js
+++ public/javascripts/models/sbml.js
@@ -1,14 +1,30 @@
 /*global define*/
 define([
+	'jquery',
 	'underscore',
 	'backbone'
-], function (_, Backbone) {
+], function ($, _, Backbone) {
 	'use strict';
 
 	var SBMLModel = Backbone.Model.extend({
-		defaults: {
-			sbml: 'was not fetched',
-			id: 'was not assigned'
+
+		validate: function (attributes, options) {
+			$.ajax({
+				data: {
+					sbml: attributes.sbml
+				},
+				type: 'POST',
+				processData: true,
+				url: 'libsbml/validate',
+				success: function (data, textStatus, jqXHR) {
+					console.log('Called libSBML Validate');
+					if (data === 'false') {
+						attributes.valid = false;
+						console.log('SBML is not valid');
+					}
+				}
+			});
+
 		}
 	});
 
