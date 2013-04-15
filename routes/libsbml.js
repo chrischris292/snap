2b5757d632f8ce98a8f527e60049aceea5a7e4b1
Stanley Gu <stanleygu@gmail.com>
2013-04-15 18:33:19 +0000

    Added fetching of param ids and values

--- routes/libsbml.js
+++ routes/libsbml.js
@@ -20,14 +20,17 @@ exports.methods = function(req, res) {
 				valid = true;
 			}
 			res.send({numErrors: numErrors, valid: valid, sbml: sbml});
-		} else if (req.params.method === 'parametersIds') {
+		} else if (req.params.method === 'parameters') {
 			var params = {};
 			params.ids = [];
+			params.values = [];
 			var parameters = libsbml.Model_getListOfParameters(model);
 			for (i = 0; i < libsbml.ListOf_size(parameters); i += 1) {
-				params.ids.push(libsbml.Model_getParameter(mod, i));
+				var parameter = (libsbml.Model_getParameter(mod, i));
+				params.ids.push(libsbml.Parameter_getId(parameter));
+				params.values.push(libsbml.Parameter_getValue(parameter));
 			}
-			console.log(params.ids);
+			console.log(params);
 			res.send(params);
 		} else if (req.params.method === 'species') {
 		} else {
